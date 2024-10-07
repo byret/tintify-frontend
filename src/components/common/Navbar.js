0a66ac27d8ba7b8d61c344a3a6a0e86b236bb281
byret <byrett@gmail.com>
2024-10-07 04:56:45 +0200

    adjustments

--- src/components/common/Navbar.js
+++ src/components/common/Navbar.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { useNavigate } from "react-router-dom";
+import { useNavigate } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 
 const Navbar = ({
@@ -12,107 +12,120 @@ const Navbar = ({
   openModal,
 }) => {
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
-  let dropdownTimer;
-
-  const handleMouseEnter = () => {
-    clearTimeout(dropdownTimer);
-    setIsDropdownOpen(true);
-  };
-
-  const handleMouseLeave = () => {
-    dropdownTimer = setTimeout(() => {
-      setIsDropdownOpen(false);
-    }, 300);
-  };
-
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
   const navigate = useNavigate();
+
+  const toggleDropdown = () => {
+    setIsDropdownOpen(!isDropdownOpen);
+  };
+
+  const toggleMobileMenu = () => {
+    setIsMobileMenuOpen(!isMobileMenuOpen);
+  };
 
   return (
     <nav className="bg-[#a38c9a] p-2 shadow-lg fixed w-full top-0 z-10">
       <div className="container mx-auto flex justify-between items-center">
         <div className="flex items-center space-x-4">
           <Link to="/">
-            <img src="/logo.png" alt="Logo" className="h-16" />
+            <img src="/logo.png" alt="Logo" className="h-10 md:h-16" />
           </Link>
           <Link to="/">
-            <span className="text-white text-2xl font-semibold font-sans">Tintify</span>
+            <span className="text-white text-xl md:text-2xl font-semibold font-sans">Tintify</span>
           </Link>
         </div>
 
-        {showSaveButton && (
-          <button
-            onClick={onSaveClick}
-            className="bg-primary text-secondary py-2 px-4 rounded transition-transform duration-200 hover:-translate-y-1"
-          >
-            Save
-          </button>
-        )}
-
-        {isAuthenticated && onHomePage ? (
-          <>
-            <div
-              className="absolute left-1/2 transform -translate-x-1/2"
-              onMouseEnter={handleMouseEnter}
-              onMouseLeave={handleMouseLeave}
+        <div className="md:hidden">
+          <button onClick={toggleMobileMenu} className="text-white">
+            <svg
+              className="w-6 h-6"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
             >
-              <span className="text-white cursor-pointer">
-                Welcome, {username}!
-              </span>
-              {isDropdownOpen && (
-                <div className="absolute right-0 mt-2 w-48 bg-white shadow-md rounded-md z-20">
-                  <Link
-                    to={`/users/${username}`}
-                    className="block px-4 py-2 text-secondary hover:bg-gray-200"
-                  >
-                    My Profile
-                  </Link>
-                  <Link
-                    to="/account-settings"
-                    className="block px-4 py-2 text-secondary hover:bg-gray-200"
-                  >
-                    Account Settings
-                  </Link>
-                  <button
-                    onClick={handleLogout}
-                    className="block w-full text-left px-4 py-2 text-secondary hover:bg-gray-200"
-                  >
-                    Log Out
-                  </button>
-                </div>
-              )}
-            </div>
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h16M4 18h16"
+              ></path>
+            </svg>
+          </button>
+        </div>
 
+        <div className={`md:flex items-center space-x-4 ${isMobileMenuOpen ? 'block' : 'hidden'}`}>
+          {showSaveButton && (
             <button
-              onClick={openModal}
+              onClick={onSaveClick}
               className="bg-primary text-secondary py-2 px-4 rounded transition-transform duration-200 hover:-translate-y-1"
             >
-              Create
+              Save
             </button>
-          </>
-        ) : !isAuthenticated && onHomePage ? (
-          <div className="flex items-center space-x-4">
-            <button
-              onClick={() => navigate("/login")}
+          )}
+
+          {isAuthenticated && onHomePage ? (
+            <>
+              <div onClick={toggleDropdown} className="relative">
+                <span className="text-white cursor-pointer">
+                  Welcome, {username}!
+                </span>
+                {isDropdownOpen && (
+                  <div className="absolute right-0 mt-2 w-48 bg-white shadow-md rounded-md z-20">
+                    <Link
+                      to={`/users/${username}`}
+                      className="block px-4 py-2 text-secondary hover:bg-gray-200"
+                    >
+                      My Profile
+                    </Link>
+                    <Link
+                      to="/account-settings"
+                      className="block px-4 py-2 text-secondary hover:bg-gray-200"
+                    >
+                      Account Settings
+                    </Link>
+                    <button
+                      onClick={handleLogout}
+                      className="block w-full text-left px-4 py-2 text-secondary hover:bg-gray-200"
+                    >
+                      Log Out
+                    </button>
+                  </div>
+                )}
+              </div>
+
+              <button
+                onClick={openModal}
                 className="bg-primary text-secondary py-2 px-4 rounded transition-transform duration-200 hover:-translate-y-1"
               >
-                 Login
-            </button>
+                Create
+              </button>
+            </>
+          ) : !isAuthenticated && onHomePage ? (
+            <div className="flex items-center space-x-4">
+              <button
+                onClick={() => navigate('/login')}
+                className="bg-primary text-secondary py-2 px-4 rounded transition-transform duration-200 hover:-translate-y-1"
+              >
+                Login
+              </button>
 
-                  <button
-                    onClick={() => navigate("/register")}
-                    className="bg-primary text-secondary py-2 px-4 rounded transition-transform duration-200 hover:-translate-y-1"
-                  >
-                    Register
-                  </button>
+              <button
+                onClick={() => navigate('/register')}
+                className="bg-primary text-secondary py-2 px-4 rounded transition-transform duration-200 hover:-translate-y-1"
+              >
+                Register
+              </button>
 
-                  <button
-                    onClick={openModal}
-                    className="bg-primary text-secondary py-2 px-4 rounded transition-transform duration-200 hover:-translate-y-1"
-                  >
-                    Create
-                  </button>
-          </div>
-        ) : null}
+              <button
+                onClick={openModal}
+                className="bg-primary text-secondary py-2 px-4 rounded transition-transform duration-200 hover:-translate-y-1"
+              >
+                Create
+              </button>
+            </div>
+          ) : null}
+        </div>
       </div>
     </nav>
   );
